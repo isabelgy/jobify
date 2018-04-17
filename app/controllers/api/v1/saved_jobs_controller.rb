8053f8e762f1b410390bb2af0d855cbc8672144d
Isabel Chan <isabelchan1994@gmail.com>
2018-04-17 10:11:13 +0800

    setting up the controller

--- app/controllers/api/v1/saved_jobs_controller.rb
+++ app/controllers/api/v1/saved_jobs_controller.rb
@@ -1,6 +1,21 @@
 class Api::V1::SavedJobsController < Api::V1::BaseController
   def create
+    @saved_jobs = SavedJob.new(saved_jobs_params)
+    @job = Job.find(@saved_jobs[:job_id])
+    @saved_jobs.
   end
+
+@rental = Rental.new(rental_params)
+    @station = Station.find(@rental[:station_id])
+    @rental.station = @station
+    @station.update(availability: false)
+
+    @user = User.find(params[:user_id])
+    @rental.user = @user
+    @rental.save
+
+
+    render json: @rental
 
   def index
 
